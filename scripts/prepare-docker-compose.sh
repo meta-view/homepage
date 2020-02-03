a05e3710eba205b49a3e5d19db73bc602786a520
Philipp Haussleiter <philipp@haussleiter.de>
2020-02-03 17:46:03 +0100

    initial commit

---
+++ scripts/prepare-docker-compose.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+cat << EOF
+version: "3"
+
+networks:
+  proxy:
+    external: true
+  internal:
+    external: false
+
+services:
+  $CI_PROJECT_NAME:
+    image: $CI_REGISTRY_IMAGE:$CI_COMMIT_SHORT_SHA
+    container_name: $CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG
+    restart: always    
+    labels:
+      - traefik.backend=$CI_PROJECT_NAME-$CI_COMMIT_REF_SLUG
+      - traefik.frontend.rule=Host:$APP_DEPLOY_URL
+      - traefik.docker.network=proxy
+      - traefik.port=$APP_PORT
+    networks:
+      - internal
+      - proxy
+EOF
